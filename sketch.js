7c39de298c34a062b80a653a6047353ca0a73481
krishna1220 <59966007+krishna1220@users.noreply.github.com>
2020-03-22 12:55:23 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,42 @@
+var ball;
+var database,position;
+function setup(){
+    createCanvas(500,500);
+    database=firebase.database();
+    ball = createSprite(250,250,10,10);
+    ball.shapeColor = "red";
+    var ballposition=database.ref('ball/position');
+    ballposition.on("value",readPosition,showError)
+}
+
+function draw(){
+    background("white");
+    if(keyDown(LEFT_ARROW)){
+        changePosition(-10,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        changePosition(10,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        changePosition(0,-10);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        changePosition(0,+10);
+    }
+    drawSprites();
+}
+
+function changePosition(x,y){
+    database.rect('ball/position').set({
+       
+        'x':position.x+x,
+        'y':position.y+y
+
+    })
+}
+function readPosition (data) {
+    position=data.val();
+    ball.x=position.x;
+    ball.y=position.y;
+
+}
